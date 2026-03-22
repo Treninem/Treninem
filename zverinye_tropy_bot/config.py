848cbf640f37cedf425190d1fa0abed2bba30a8b
Treninem <trenin.98@gmail.com>
2026-03-23 01:05:11 +0300

    Delete zverinye_tropy_bot directory

--- zverinye_tropy_bot/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
-DATABASE_URL = os.getenv('DATABASE_URL')
-ADMIN_IDS = [2097006037]  # ID администраторов
