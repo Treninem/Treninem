e43e3a81dc8bb9fc1a792d9dabd955b82699f272
Treninem <trenin.98@gmail.com>
2026-04-13 22:16:10 +0300

    Delete features directory

--- features/points_system/calculator.py
+++
@@ -1,8 +0,0 @@
-"""Расчет баллов по действиям."""
-from __future__ import annotations
-
-from config.constants import POINT_EVENTS
-
-
-def get_points_for_event(event_name: str) -> int:
-    return POINT_EVENTS.get(event_name, 0)
