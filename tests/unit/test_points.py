e9c477dd527f0905c91f3b1a996d49c450c6aaec
Treninem <trenin.98@gmail.com>
2026-04-13 23:39:32 +0300

    Delete tests directory

--- tests/unit/test_points.py
+++
@@ -1,9 +0,0 @@
-from features.points_system.rank_updater import get_rank_for_points
-
-
-def test_get_rank_for_points():
-    assert get_rank_for_points(0) == "Новичок"
-    assert get_rank_for_points(150) == "Стрелок"
-    assert get_rank_for_points(450) == "Тактик"
-    assert get_rank_for_points(800) == "Ветеран"
-    assert get_rank_for_points(1200) == "Легенда"
